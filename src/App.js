1336686a4b778d16d7ff235415c52e97ea78cc22
MESNAOU <mesnaoui2002@outlook.fr>
2024-02-22 21:08:30 +0100

    version 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+
+import Contact from './components/Contact';
+import Home from './components/Home';
+import About from './components/About';
+import Blog from './components/Blog';
+import BlogDetail from './components/BlogDetail';
+import FlatDetail from './components/FlatDetail';
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header/>
+        <Routes>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/contact"  element={<Contact/>}></Route>
+          <Route path="/about"  element={<About/>}></Route>
+          <Route path="/blog" element={<Blog/>}></Route>
+          <Route path="/blog/:id"  element={<BlogDetail/>}></Route>
+          <Route path="/flat/:slug"  element={<FlatDetail/>}></Route>
+        </Routes>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
